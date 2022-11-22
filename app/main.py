e3ae0c20dfbe384730ac94e69ebd49c067bc2ef2
Frodenils <nils.frd@gmail.com>
2022-11-22 16:18:43 +0100

    ✨ Added Delete Utilisateur

--- app/main.py
+++ app/main.py
@@ -28,15 +28,15 @@ def create_utilisateur(utilisateur: schemas.UtilisateurCreate, db: Session = Dep
     return crud.create_utilisateur(db=db, utilisateur=utilisateur)
 
 
-@app.get("/utilisateur/", response_model=List[schemas.UtilisateurModel])
-def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    utilisateur = crud.get_users(db, skip=skip, limit=limit)
+@app.get("/utilisateurs/", response_model=List[schemas.UtilisateurModel])
+def read_utilisateurs(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    utilisateur = crud.get_utilisateurs(db, skip=skip, limit=limit)
     return utilisateur
 
 
 @app.get("/utilisateur/{id_utilisateur}", response_model=schemas.UtilisateurModel)
-def read_user(id_utilisateur: int, db: Session = Depends(get_db)):
-    db_user = crud.get_user(db, id_utilisateur=id_utilisateur)
+def read_utilisateur(id_utilisateur: int, db: Session = Depends(get_db)):
+    db_user = crud.get_utilisateur(db, id_utilisateur)
     if db_user is None:
         raise HTTPException(status_code=404, detail="Utilisateur not found")
     return db_user
@@ -58,19 +58,18 @@ def create_publication_for_utilisateur(
     return crud.create_utilisateur(db=db, Publication=Publication, id_utilisateur=id_utilisateur)
 
 
-@app.get("/publications/", response_model=List[schemas.Publication])
+@app.get("/publications/", response_model=List[schemas.PublicationModel])
 def read_Publications(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     Publications = crud.get_Publications(db, skip=skip, limit=limit)
     return Publications
 
 
-@app.delete("/utilisateur/delete/{user_id}")
-def read_user(user_id: int, db: Session = Depends(get_db)):
-    db_user = crud.get_user(db, user_id=user_id)
-    with Session(engine) as session:
-        db_user = crud.get_user(db, user_id=user_id)
-        if not db_user:
+@app.delete("/utilisateur/{id_utilisateur}")
+def read_utilisateur(id_utilisateur: int, db: Session = Depends(get_db)):
+    db_utilisateur = crud.get_utilisateur(db, id_utilisateur)
+    with db as session:
+        if not db_utilisateur:
             raise HTTPException(status_code=404, detail="Utilisateur not found")
-        session.delete(db_user)
+        session.delete(db_utilisateur)
         session.commit()
         return {"ok": True}
